4929739f06c35edb9bf285cea611f0c1c84e7039
FidelissJr <andrefidelisjr@hotmail.com>
2023-02-25 16:41:50 -0300

    Created my own image based on python and tested and tested 3 workers service running

--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -5,6 +5,7 @@ from random import randint
 
 if __name__ == '__main__':
     r = redis.Redis(host='queue', port=6379, db=0)
+    print('Waiting messages...')
     while True:
         message = json.loads(r.blpop('sender')[1])
         print('Sending the Subject:', message['subject'])
