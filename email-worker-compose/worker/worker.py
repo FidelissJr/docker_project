77aba6a4a3a2ca386201c01d697c39e19d0c0e57
FidelissJr <andrefidelisjr@hotmail.com>
2023-02-25 16:30:52 -0300

    created the queue service and de worker.py file to simulate the sending email

---
+++ email-worker-compose/worker/worker.py
@@ -0,0 +1,12 @@
+import redis
+import json
+from time import sleep
+from random import randint
+
+if __name__ == '__main__':
+    r = redis.Redis(host='queue', port=6379, db=0)
+    while True:
+        message = json.loads(r.blpop('sender')[1])
+        print('Sending the Subject:', message['subject'])
+        sleep(randint(15, 45))
+        print('Message', message['message'], 'enviada')
